2d191af900eb2cb570f5dc2a16a8298a393919e1
kunal89204 <kunalkhandelwal108@gmail.com>
2025-07-14 22:05:35 +0530

    changes

--- sikeit.py
+++ sikeit.py
@@ -51,7 +51,7 @@ def index():
         except Exception as e:
             result['error'] = str(e)
 
-    return render_template('index.html', result=result)
+    return render_template('./index.html', result=result)
 
 if __name__ == '__main__':
     app.run(debug=True)
