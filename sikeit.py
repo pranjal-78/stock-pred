750570d683b07d3bdc539227002f7c407d1baabe
kunal89204 <kunalkhandelwal108@gmail.com>
2025-07-14 22:20:26 +0530

    made it deployabel

--- sikeit.py
+++ sikeit.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
 from datetime import datetime, timedelta
+import os
 
 app = Flask(__name__)
 
@@ -54,4 +55,5 @@ def index():
     return render_template('./index.html', result=result)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
